ebe7e85a262c6d5878ed06b8a1e74f699048f808
Nitweeg <maxime.tredez@hotmail.fr>
2021-11-16 16:57:49 +0100

    NavBar / Connexion & Inscription Close

--- View/Connexion.php
+++ View/Connexion.php
@@ -8,62 +8,50 @@
     <script type="text/javascript" src="includes/Connect.js"></script>
   </head>
   <body>
-    <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
     <div class="container" id="container">
     	<div class="form-container sign-up-container">
     		<form action="../Controller/Inscription.php" method="post">
-    			<h1>Create Account</h1>
+    			<h1>Création Compte</h1>
     			<div class="social-container">
     				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
     				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
     				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
     			</div>
-    			<span>or use your email for registration</span>
           <input type="text" name="Nom" placeholder="Nom" required>
     			<input type="text" placeholder="Prenom" name="Prenom" />
     			<input type="email" name="Mail" placeholder="G.Sapone@mail.fr" required>
     			<input type="password" placeholder="Password" name="MDP" required />
-    			<button>Sign Up</button>
+    			<button>Inscription</button>
     		</form>
     	</div>
     	<div class="form-container sign-in-container">
     		<form action="../Controller/Connexion.php" method="post">
-    			<h1>Sign in</h1>
+    			<h1>Connexion</h1>
     			<div class="social-container">
     				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
     				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
     				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
     			</div>
-    			<span>or use your account</span>
     			<input type="email" placeholder="Email" name="identifiant" required/>
     			<input type="password" placeholder="Password" name="pswd" required />
-    			<a href="#">Forgot your password?</a>
-    			<button>Sign In</button>
+    			<a href="#">Mot de passe oublié ?</a>
+    			<button>Connexion</button>
     		</form>
     	</div>
     	<div class="overlay-container">
     		<div class="overlay">
     			<div class="overlay-panel overlay-left">
-    				<h1>Welcome Back!</h1>
-    				<p>To keep connected with us please login with your personal info</p>
-    				<button class="ghost" id="signIn">Sign In</button>
+    				<h1>Après ton inscription</h1>
+    				<p>Connecte toi maintenant ! Des nouvelles promos chaques semaine et de nouveaux produits !</p>
+    				<button class="ghost" id="signIn">Connexion</button>
     			</div>
     			<div class="overlay-panel overlay-right">
-    				<h1>Hello, Friend!</h1>
-    				<p>Enter your personal details and start journey with us</p>
-    				<button class="ghost" id="signUp">Sign Up</button>
+    				<h1>Bienvenue !</h1>
+    				<p>Créer ton compte afin d'avoir accès à tout nos produits et promotions en ligne!</p>
+    				<button class="ghost" id="signUp">Inscription</button>
     			</div>
     		</div>
     	</div>
     </div>
-
-    <footer>
-    	<p>
-    		Created with <i class="fa fa-heart"></i> by
-    		<a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-    		- Read how I created this and how you can join the challenge
-    		<a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
-    	</p>
-    </footer>
   </body>
 </html>
--- View/includes/header.php
+++ View/includes/header.php
@@ -3,8 +3,76 @@
   <head>
     <meta charset="utf-8">
     <title> ~Projet Meccano~ </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://mdbootstrap.com/wp-content/themes/mdbootstrap4/css/mdb5/3.10.0/compiled.min.css">
+    <link rel="stylesheet" href="https://mdbootstrap.com/api/snippets/static/download/MDB5-Pro-Advanced_3.3.0/plugins/css/all.min.css">
+    <link rel="stylesheet" id="roboto-subset.css-css" href="https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/css/mdb5/fonts/roboto-subset.css?ver=3.9.0-update.5" type="text/css" media="all">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../CSS/main.css" type="text/css">
   </head>
   <body>
-    <h1 id="Accueil"> Projet Meccano</h1>
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#">
+      <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="30" alt="" loading="lazy" />
+    </a>
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Promotions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Liste Produits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tutoriels</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+
+      <!-- Search form -->
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <!-- Navbar dropdown -->
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle hidden-arrow" href="#" id="navbarDropdown" role="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fas fa-shopping-cart"></i>
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <img src="../images/util.png" class="rounded-circle img-fluid"
+              height='25' width='25'>
+          </a>
+          <!-- Dropdown menu -->
+          <ul class="dropdown-menu dropdown-menu-end p-1" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Your Account</a></li>
+            <li><a class="dropdown-item" href="#">Log Out</a></li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
   </body>
 </html>
