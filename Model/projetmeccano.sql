2a8c11ad540a2cbca73f7f3d8b7e8a9d9323072a
SAPONE <guillaumesaponepro@gmail.com>
2021-11-16 15:03:05 +0100

    MAJ BDD2

--- Model/projetmeccano.sql
+++ Model/projetmeccano.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 16 nov. 2021 à 13:59
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `projetmeccano`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `panier`
---
-
 DROP TABLE IF EXISTS `panier`;
 CREATE TABLE IF NOT EXISTS `panier` (
   `idPanier` int(11) NOT NULL AUTO_INCREMENT,
@@ -35,14 +6,7 @@ CREATE TABLE IF NOT EXISTS `panier` (
   `idUser` int(11) NOT NULL,
   PRIMARY KEY (`idPanier`),
   KEY `idUser` (`idUser`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `produit`
---
-
+) ;
 DROP TABLE IF EXISTS `produit`;
 CREATE TABLE IF NOT EXISTS `produit` (
   `idProduit` int(11) NOT NULL AUTO_INCREMENT,
@@ -50,21 +14,7 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `Price` double NOT NULL,
   `Description` char(255) NOT NULL,
   PRIMARY KEY (`idProduit`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `produit`
---
-
-INSERT INTO `produit` (`idProduit`, `ProdName`, `Price`, `Description`) VALUES
-(1, 'izufpç_&quot;\'_tf', 5, 'zkiuef');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur`
---
-
+);
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
@@ -72,17 +22,4 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `MotDePasse` text NOT NULL,
   `Mail` varchar(70) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `utilisateur`
---
-
-INSERT INTO `utilisateur` (`idUser`, `UserName`, `MotDePasse`, `Mail`) VALUES
-(1, 'Sapone Guillaume', 'c649a38986ad94059e9a471f5e82d23c9c5f3ba5b261f3467eccccaa8db5277e', 'G.Sapone@mail.fr'),
-(2, 'test testNom', 'a39f0a6e0b5e26f8e6ee8ccebb62e21024cb78c907b30720dd18e45423a56038', 'S.Gui@mail.com');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+);
