258478665a995c44f25274c23cc5fca99e4e61a0
SAPONE <guillaumesaponepro@gmail.com>
2021-12-08 13:10:13 +0100

    uyv

--- Controller/bddConnect.php
+++ Controller/bddConnect.php
@@ -1,6 +1,7 @@
 <?php
   function Connect(){
     try{
+      echo "string";
       $connexion = new PDO('mysql:host=localhost;dbname=projetmeccano;charset=utf8', 'root', '1808');
       return $connexion;
     }catch(Exception $e){
