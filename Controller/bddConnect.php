74bb7551825ef87b8e319d517ef4dcd08bf9a22c
Nitweeg <maxime.tredez@hotmail.fr>
2021-12-08 15:41:06 +0100

    CSSAfficheProduct

--- Controller/bddConnect.php
+++ Controller/bddConnect.php
@@ -1,7 +1,7 @@
 <?php
   function Connect(){
     try{
-      $connexion = new PDO('mysql:host=localhost;dbname=projetmeccano;charset=utf8', 'root', '1808');
+      $connexion = new PDO('mysql:host=localhost:8889;dbname=Projetmeccano;charset=utf8', 'root', 'root');
       return $connexion;
     }catch(Exception $e){
       die('Erreur : ' .$e->getMessage());
--- Controller/function/setProduit.php
+++ Controller/function/setProduit.php
@@ -2,13 +2,12 @@
   include('../Controller/getInfos.php');
   include('includes/header.php');
   if(isConnected()){
-    echo "<br><br><br><br>".'Session is ON';
+
   }else{
-    echo "<br><br><br><br>".'Session is OFF';
+
   }
 
   $reSearch = $_POST['research'];
-  echo $reSearch;
 
   function setPage(){
     $reSearch = $_POST['research'];
@@ -16,17 +15,49 @@
     $img = getImage($reSearch);
     $desc = getDesc($reSearch);
     $price = getPrice($reSearch);
-    echo "<div>";
-    echo "<h2>".$name."</h2>";
-    echo "<img src='".$img."' alt=".$name.">";
-    echo "<p>".$desc."</p>";
-    echo "<p>".$price."</p>";
-    echo "</div>";
-    echo "<form class='setProduct' action='../Controller/addPanier.php' method='post'>";
-    echo "<input id=idUser name=Userid type=hidden value=".$_SESSION['idUser'].">";
-    echo "<input id=idProd name=Prodid type=hidden value=".getID($reSearch).">";
-    echo "<input id=Quantity name=ProdQuantity type=number min=0>";
-    echo "<input type=submit name=addPanier value=ajouter>";
-    echo "</form>";
+    echo "<section class='Bienvenue'>";
+    ?>
+    <div class="card">
+	<div class="row no-gutters">
+		<aside class="col-md-6">
+<article class="gallery-wrap">
+	<div class="img-big-wrap">
+	   <a href="#"><img src="<?php echo $img ?>"></a>
+	</div> <!-- img-big-wrap.// -->
+</article> <!-- gallery-wrap .end// -->
+		</aside>
+		<main class="col-md-6 border-left">
+<article class="content-body">
+
+<h2 class="title"><?php echo $name ?></h2>
+
+<div class="mb-3">
+	<var class="price h4"><?php echo $price ?></var>
+	<span class="text-muted">/€</span>
+</div>
+
+<p><?php echo $desc ?></p>
+
+
+<hr>
+	<div class="row">
+		<div class="form-group col-md flex-grow-0">
+			<label>Quantity</label>
+			<div class="input-group mb-3 input-spinner">
+        <form class='setProduct' action='../Controller/addPanier.php' method='post'>
+          <input id=idUser name=Userid type=hidden value=".$_SESSION['idUser'].">
+          <input id=idProd name=Prodid type=hidden value=".getID($reSearch).">
+			    <input id=Quantity name=ProdQuantity type=number min=0>
+			</div>
+		</div> <!-- col.// -->
+	</div> <!-- row.// -->
+	<input type=submit name=addPanier value=ajouter class="btn btn-outline-primary">
+  </form>
+</article> <!-- product-info-aside .// -->
+		</main> <!-- col.// -->
+	</div> <!-- row.// -->
+</div>
+<?php
+    echo "</section>";
   }
  ?>
