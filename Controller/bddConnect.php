8599168316cc08fcee88ab8206a0e8973698a886
SAPONE <guillaumesaponepro@gmail.com>
2021-12-08 13:10:28 +0100

    rdxctfvygbuhnji,ok

--- Controller/bddConnect.php
+++ Controller/bddConnect.php
@@ -1,7 +1,6 @@
 <?php
   function Connect(){
     try{
-      echo "string";
       $connexion = new PDO('mysql:host=localhost;dbname=projetmeccano;charset=utf8', 'root', '1808');
       return $connexion;
     }catch(Exception $e){
